f2d54040b444e85f9a78285df0d02ca28dd19c66
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-22 10:43:15 +0300

    Calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,99 +4,70 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.calculator.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tilFirstInput: TextInputLayout
-    lateinit var etFirstInput: TextInputEditText
-    lateinit var tilSecondInput: TextInputLayout
-    lateinit var etSecondInput: TextInputEditText
-    lateinit var btnAdd: Button
-    lateinit var btnSubtract: Button
-    lateinit var btnMultiply: Button
-    lateinit var btnDivide: Button
-    lateinit var tvResult: TextView
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        castViews()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 
     override fun onResume() {
+
         super.onResume()
-        btnAdd.setOnClickListener {
-            addition()
-//            validateApp()
-//            clearErrors()
-        }
-        btnSubtract.setOnClickListener {
-            subtraction()
-//            validateApp()
-//            clearErrors()
-        }
-        btnMultiply.setOnClickListener {
-            multiplication()
-//            validateApp()
-//            clearErrors()
-        }
-        btnDivide.setOnClickListener {
-            division()
-//            validateApp()
-//            clearErrors()
-        }
+        binding.btnAdd.setOnClickListener { calculate("+") }
+        binding.btnSubtract.setOnClickListener { calculate("-") }
+        binding.btnMultiply.setOnClickListener { calculate("*") }
+        binding.btnDivide.setOnClickListener { calculate("%") }
     }
 
-    fun castViews(){
-        tilFirstInput = findViewById(R.id.tilFirstInput)
-        etFirstInput = findViewById(R.id.etFirstInput)
-        tilSecondInput = findViewById(R.id.tilSecondInput)
-        etSecondInput = findViewById(R.id.etSecondInput)
-        btnAdd = findViewById(R.id.btnAdd)
-        btnSubtract = findViewById(R.id.btnSubtract)
-        btnMultiply = findViewById(R.id.btnMultiply)
-        btnDivide = findViewById(R.id.btnDivide)
-        tvResult = findViewById(R.id.tvResult)
+    fun calculate(operation: String) {
+        clearErrors()
+        val num1 = binding.etFirstInput.text.toString()
+        val num2 = binding.etSecondInput.text.toString()
+        var error = false
+
+        if (num1.isBlank()) {
+            binding.tilFirstInput.error = "Num1 is required"
+            error = true
+
+        }
+        if (num2.isBlank()) {
+            binding.tilFirstInput.error = "Num2 is required"
+            error = true
+
+
+        }
+        if (!error) {
+            val firstNum = num1.toDouble()
+            val secondNum = num2.toDouble()
+
+
+            val result = when (operation) {
+                "+" -> firstNum + secondNum
+                "-" -> firstNum + secondNum
+                "*" -> firstNum * secondNum
+                "%" -> {
+                    firstNum % secondNum
+                }
+                else -> throw Exception("Invalid operator")
+
+            }
+            binding.tvResult.text = result.toString()
+        }
 
     }
-    fun addition(){
-        val num1 = etFirstInput.text.toString().toInt()
-        val num2 = etSecondInput.text.toString().toInt()
-        val finalResult = num1 + num2
-        tvResult.text = "Result: $finalResult"
+
+    fun clearErrors(){
+        binding.tilFirstInput.error=null
+        binding.tilSecondInput.error=null
+
 
     }
-    fun subtraction(){
-        val num1 = etFirstInput.text.toString().toInt()
-        val num2 = etSecondInput.text.toString().toInt()
-        val finalResult = num1 - num2
-        tvResult.text = "Result: $finalResult"
-    }
-    fun multiplication(){
-        val num1 = etFirstInput.text.toString().toInt()
-        val num2 = etSecondInput.text.toString().toInt()
-        val finalResult = num1 * num2
-        tvResult.text = "Result: $finalResult"
-    }
-    fun division(){
-        val num1 = etFirstInput.text.toString().toInt()
-        val num2 = etSecondInput.text.toString().toInt()
-        val finalResult = num1 / num2
-        tvResult.text = "Result: $finalResult"
-    }
-//    fun validateApp(){
-//        val firstinput = etFirstInput.text.toString()
-//        val secondinput = etSecondInput.text.toString()
-//
-//        if (firstinput.isBlank() || secondinput.isBlank()){
-//            tvResult.text = "Result: "
-//            tvResult.error = "Please enter valid numbers."
-//        }
-//
-//    }
-//    fun clearErrors(){
-//        tilFirstInput.error = null
-//        tilSecondInput.error = null
-//    }
 }
